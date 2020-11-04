c4bf0b497a6ebf24d111fcdbfdd7044925716693
bakaneko <notbakaneko@users.noreply.github.com>
2020-11-04 21:14:36 +0900

    send new chat message event over websocket

---
+++ app/Events/ChatMessageEvent.php
@@ -0,0 +1,54 @@
+<?php
+
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace App\Events;
+
+use App\Models\Chat\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class ChatMessageEvent implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    public $broadcastQueue;
+    public $message;
+    public $userId;
+
+    public function __construct(Message $message, $userId)
+    {
+        // TODO: different queue? conditional queue?
+        $this->broadcastQueue = config('osu.notification.queue_name');
+
+        // TODO: avoid serializing so handle doesn't need to perform queries to deserialize.
+        $this->message = $message;
+        $this->userId = $userId;
+    }
+
+    public function broadcastAs()
+    {
+        return 'chat.message.new';
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        if ($this->message->channel->isPublic()) {
+            return new Channel("chat:channel:{$this->message->channel->getKey()}");
+        } else {
+            return new Channel("private:user:{$this->userId}");
+        }
+    }
+
+    public function broadcastWith()
+    {
+        return $this->message->toArray();
+    }
+}
