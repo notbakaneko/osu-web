ad9787fe86307fe84c927c230f65a4d3cdc5ba83
bakaneko <notbakaneko@users.noreply.github.com>
2020-11-13 21:39:31 +0900

    handle duplicate calls to Channel.load

--- resources/assets/lib/models/chat/channel.ts
+++ resources/assets/lib/models/chat/channel.ts
@@ -2,7 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 import ChatAPI from 'chat/chat-api';
-import { ChannelJson, ChannelJsonExtended, ChannelType, MessageJson } from 'chat/chat-api-responses';
+import { ChannelJson, ChannelJsonExtended, ChannelType, GetChannelJson, MessageJson } from 'chat/chat-api-responses';
 import * as _ from 'lodash';
 import { action, computed, observable } from 'mobx';
 import User from 'models/user';
@@ -26,6 +26,10 @@ export default class Channel {
   @observable type: ChannelType = 'NEW';
   @observable users: number[] = [];
 
+  private pendingRequests = {
+    load: null as JQuery.jqXHR<GetChannelJson> | null,
+  };
+
   @computed
   get firstMessage() {
     return this.messages.length > 0 ? this.messages[0] : undefined;
@@ -34,6 +38,15 @@ export default class Channel {
   @computed
   get hasEarlierMessages() {
     return this.firstMessageId !== this.minMessageId;
+  }
+
+  /**
+   * Minimum required to display the channel.
+   */
+  @computed
+  get isDisplayable() {
+    return this.name.length > 0
+      && this.icon != null;
   }
 
   @computed
@@ -48,15 +61,6 @@ export default class Channel {
   @computed
   get lastMessage(): Message | undefined {
     return this.messages[this.messages.length - 1];
-  }
-
-  /**
-   * Minimum required to display the channel.
-   */
-  @computed
-  get metaLoaded() {
-    return this.name.length > 0
-      && this.icon != null;
   }
 
   @computed
@@ -151,18 +155,24 @@ export default class Channel {
 
   @action
   // TODO: don't pass api through
-  load(api: ChatAPI) {
-    if (this.metaLoaded || this.loading || this.newPmChannel) {
+  async load(api: ChatAPI) {
+    if (this.isDisplayable || this.newPmChannel) {
       return;
     }
 
-    this.loading = true;
-    // this isn't returned because the early return above makes it messy for promise handlers to await properly.
-    api.getChannel(this.channelId).then((response) => {
-      this.updateWithJson(response.channel);
-    }).always(action(() => {
-      this.loading = false;
-    }));
+    if (this.pendingRequests.load == null) {
+      this.pendingRequests.load = api.getChannel(this.channelId);
+      this.loading = true;
+
+      this.pendingRequests.load.then((response) => {
+        this.updateWithJson(response.channel);
+      }).always(action(() => {
+        this.loading = false;
+        this.pendingRequests.load = null;
+      }));
+    }
+
+    return this.pendingRequests.load;
   }
 
   @action
