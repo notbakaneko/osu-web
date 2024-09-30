3b094f1939b36175c19485a8c6d139d078c9cc59
bakaneko <notbakaneko@users.noreply.github.com>
2024-09-30 19:59:48 +0900

    separate method for shopify checkout id

--- resources/views/store/orders/index.blade.php
+++ resources/views/store/orders/index.blade.php
@@ -50,7 +50,7 @@
                             class="js-store-resume-checkout btn-osu-big btn-osu-big--rounded-thin"
                             data-order-id="{{ $order->getKey() }}"
                             data-provider="{{ $order->getPaymentProvider() }}"
-                            data-provider-reference="{{ $order->getProviderReference() }}"
+                            data-provider-reference="{{ $order->getShopifyCheckoutId() }}"
                             data-status="{{ $order->status }}"
                         >
                             {{ $order->isPaymentRequested() ? osu_trans('store.order.resume') : osu_trans('store.order.invoice') }}
