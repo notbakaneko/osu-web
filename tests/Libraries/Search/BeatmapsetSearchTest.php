636d00e91d7aa548830fab0b12f5a8506075954d
bakaneko <notbakaneko@users.noreply.github.com>
2025-07-31 15:02:08 +0900

    mania key filter test

---
+++ tests/Libraries/Search/BeatmapsetSearchTest.php
@@ -0,0 +1,58 @@
+<?php
+
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace Tests\Libraries\Search;
+
+use App\Libraries\Elasticsearch\Es;
+use App\Libraries\Search\BeatmapsetQueryParser;
+use App\Libraries\Search\BeatmapsetSearch;
+use App\Libraries\Search\BeatmapsetSearchParams;
+use App\Libraries\Search\BeatmapsetSearchRequestParams;
+use App\Models\Beatmap;
+use App\Models\Beatmapset;
+use App\Models\BeatmapTag;
+use Carbon\CarbonImmutable;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Tests\TestCase;
+
+class BeatmapsetSearchTest extends TestCase
+{
+    public function testManiaKeysFilter()
+    {
+        $beatmapsetFactory = Beatmapset::factory()->ranked();
+        $beatmapFactory = Beatmap::factory()->ruleset('mania')->state(['approved' => Beatmapset::STATES['ranked']]);
+
+        // TODO: map with 7 and 4 key
+        $beatmapsetFactory->withBeatmaps('osu')->create();
+        $beatmap1 = $beatmapsetFactory->has($beatmapFactory->state(['diff_size' => 7]))->create();
+        $beatmap2 = $beatmapsetFactory->has($beatmapFactory->state(['diff_size' => 4]))->create();
+        $this->refresh();
+        $this->assertCount(3, new BeatmapsetSearch()->response()->ids());
+
+        $this->assertSame([(string) $beatmap1->getKey()], new BeatmapsetSearch(new BeatmapsetSearchRequestParams([
+            'q' => "keys=7"
+        ]))->response()->ids());
+        $this->assertSame([(string) $beatmap2->getKey()], new BeatmapsetSearch(new BeatmapsetSearchRequestParams([
+            'q' => "-keys=7"
+        ]))->response()->ids());
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        config_set('osu.beatmapset.guest_advanced_search', true);
+        static::deleteAllEsBeatmapsets();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    private function refresh(): void
+    {
+        Es::getClient()->indices()->refresh();
+    }
+}
