b98dbcf4c9a1f9fa702cd27627cce1011fa95ba2
nanaya <me@nanaya.net>
2026-08-26 19:23:17 +0900

    Update beatmap pack installation guide for lazer

--- resources/lang/en/beatmappacks.php
+++ resources/lang/en/beatmappacks.php
@@ -12,7 +12,7 @@ return [
 
         'blurb' => [
             'important' => 'READ THIS BEFORE DOWNLOADING',
-            'install_instruction' => 'Installation: Once a pack has been downloaded, extract the contents of the pack into your osu! Songs directory and osu! will do the rest.',
+            'install_instruction' => 'Installation: Once a pack has been downloaded, extract the contents of the pack into a folder, select the osz files and then drag and drop them to a running osu! instance.',
         ],
     ],
 
